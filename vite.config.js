4bb9eb9e3ecd57facbf82acf2f03fc3cddc24cb4
minhpv <28213674+pvminh17@users.noreply.github.com>
2026-01-09 15:05:27 +0700

    cm

--- vite.config.js
+++ vite.config.js
@@ -17,16 +17,20 @@ export default defineConfig({
         theme_color: '#4CAF50',
         background_color: '#ffffff',
         display: 'standalone',
+        start_url: '/',
+        scope: '/',
         icons: [
           {
             src: '/icons/192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/icons/512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       },
